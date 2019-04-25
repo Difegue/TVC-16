d2c735a1ebac1e8e1ead41d1fa971b7a0997f0b4
difegue <sugoi@cock.li>
2019-04-25 21:09:18 +0200

    a

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.tvc-16.science'
+SITEURL = 'https://tvc-16.science'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
